c0618aef2d1046513249022c0859b265c11b91d5
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2022-06-06 01:16:48 -0500

    .

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import Contact from "./components/Contact";
 import About from "./components/About";
 
 let routes = (
-  <Router basename="/calendar">
+  <Router basename="/edurise-web">
     <Routes>
-      <Route exact path="/edurise-web/" element={<Landing />} />
+      <Route exact path="/edurise-web" element={<Landing />} />
       <Route exact path="/edurise-web/projects" element={<Projects />} />
       <Route exact path="/edurise-web/social" element={<Social />} />
       <Route exact path="/edurise-web/contact" element={<Contact />} />
       <Route exact path="/edurise-web/about" element={<About />} />
-      {/* <Route path="*" element={<h1>Oops...</h1>} /> */}
+      <Route path="*" element={<h1>Oops...</h1>} />
     </Routes>
   </Router>
 );
