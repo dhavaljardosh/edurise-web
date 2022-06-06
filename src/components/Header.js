63a19ef7ed2188a1254efd4013c252610e5aa91a
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2022-06-06 01:48:52 -0500

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,14 +20,6 @@ export default function Header() {
         >
           <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
             <div className="flex items-center justify-between w-full md:w-auto">
-              <a href="#">
-                <span className="sr-only">Workflow</span>
-                <img
-                  alt="Workflow"
-                  className="h-8 w-auto sm:h-10"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                />
-              </a>
               <div className="-mr-2 flex items-center md:hidden">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">Open main menu</span>
